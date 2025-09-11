6c14c21cf7294257e26e291bd730fd84ace49ad0
zZWinterZz <ryanwinter1989@gmail.com>
2025-09-11 16:14:36 +0100

    Refactor settings.py to use environment variables for SECRET_KEY, DEBUG, ALLOWED_HOSTS, and CSRF_TRUSTED_ORIGINS

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,12 +24,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5o$xcs76qodq1l0hqlc1tngw=!_=21+bd7&e$da5=+m$!uh2lq'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG"), False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = []
+CSRF_TRUSTED_ORIGINS = []
+host = os.environ.get("HOST")
+if host:
+    ALLOWED_HOSTS.append(host)
+    CSRF_TRUSTED_ORIGINS.append(f"https://{host}")
+
 
 
 # Application definition
