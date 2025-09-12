5d23721859b3cf9f67b59d86cf64613604cd7059
zZWinterZz <ryanwinter1989@gmail.com>
2025-09-12 11:58:58 +0100

    Add updated_on  to Post model with corresponding migrations

--- blog/models.py
+++ blog/models.py
@@ -11,5 +11,6 @@ class Post(models.Model):
 )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
