51bdb5a9e23d735c673940338c7b69c0eca4be33
zZWinterZz <ryanwinter1989@gmail.com>
2025-09-12 13:26:08 +0100

    Refactor import statements in admin.py to consolidate model imports

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import Post
-from .models import Comment
+from .models import Post, Comment
 
 # Register your models here.
 admin.site.register(Post)
