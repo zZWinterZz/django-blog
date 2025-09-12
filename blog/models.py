2a117b7f914d1c8ef60371422f53b196a4ecd0a5
zZWinterZz <ryanwinter1989@gmail.com>
2025-09-12 13:20:18 +0100

    Add Comment model and register it in admin; create migration for comments

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,11 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
