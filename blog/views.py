97e8ec7a60c822d65d084157bf286db731f2a23c
zZWinterZz <ryanwinter1989@gmail.com>
2025-09-12 14:25:28 +0100

    Update PostList queryset to filter published posts

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from django.views import generic
 from .models import Post
 
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
