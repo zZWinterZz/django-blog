ef40111b38c9183994e15e3eda3634877804e78c
zZWinterZz <ryanwinter1989@gmail.com>
2025-09-17 16:42:20 +0100

    Add email templates and views for account management features
    - Created base email message template for account-related emails.
    - Implemented email confirmation message and subject templates.
    - Added password reset email templates and messages.
    - Developed templates for handling unknown accounts during password reset requests.
    - Introduced email change and confirmation views with corresponding templates.
    - Added user feedback messages for various account actions (e.g., email confirmation, password changes).
    - Implemented multi-factor authentication (MFA) templates for user account security.
    - Created OpenID and social account templates for third-party authentication.
    - Enhanced user experience with informative messages and structured forms for account management.

--- codestar/settings.py
+++ codestar/settings.py
@@ -48,10 +48,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'blog',
     'django_summernote',
     'about',
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -62,6 +70,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'codestar.urls'
@@ -116,6 +125,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- codestar/urls.py
+++ codestar/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls"), name="blog-urls"),
     path("about/", include("about.urls"), name="about-urls"),
+    path("accounts/", include("allauth.urls")),
     path('summernote/', include('django_summernote.urls')),
 ]
