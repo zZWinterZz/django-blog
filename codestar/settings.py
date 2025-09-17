746011ec758eed16aae8bc478e2e04c156013289
zZWinterZz <ryanwinter1989@gmail.com>
2025-09-17 16:17:58 +0100

    Add About section with model, views, and templates; update URLs and settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'django_summernote',
+    'about',
 ]
 
 MIDDLEWARE = [
--- codestar/urls.py
+++ codestar/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls"), name="blog-urls"),
+    path("about/", include("about.urls"), name="about-urls"),
     path('summernote/', include('django_summernote.urls')),
 ]
