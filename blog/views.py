3f86a6e0bd9a6a4afc548c4fcf2163e63ec27316
zZWinterZz <ryanwinter1989@gmail.com>
2025-09-18 10:01:34 +0100

    Add comment functionality to post detail view with form handling and messages

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
+from .forms import CommentForm
+from django.contrib import messages
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
@@ -26,6 +28,19 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
+    if request.method == "POST":
+     comment_form = CommentForm(data=request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.author = request.user
+        comment.post = post
+        comment.save()
+        messages.add_message(
+        request, messages.SUCCESS,
+        'Comment submitted and awaiting approval'
+    )
+
+    comment_form = CommentForm()
 
     return render(
         request,
@@ -34,5 +49,6 @@ def post_detail(request, slug):
             "post": post,
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form,
         },
     )
