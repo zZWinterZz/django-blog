93d91af9d5e0e94d3556682b5199fb5241314caf
zZWinterZz <ryanwinter1989@gmail.com>
2025-09-18 16:10:15 +0100

    Add tests for CollaborateForm and CommentForm, and enhance view tests for collaboration requests

--- codestar/settings.py
+++ codestar/settings.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 import os
+import sys
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -103,6 +104,8 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
