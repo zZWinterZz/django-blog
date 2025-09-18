f73cad8742b1ae11fc0ed408ff2dfbf34bd21ead
zZWinterZz <ryanwinter1989@gmail.com>
2025-09-18 13:38:09 +0100

    Add CollaborateRequest model, form, and admin registration; update About view and template

--- about/views.py
+++ about/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from .models import About
+from .forms import CollaborateForm
 
 
 def about_me(request):
-    """
-    Renders the About page
-    """
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "collaborate_form": collaborate_form
+        },
     )
 
